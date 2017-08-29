d1b0268dcc338c1ca92ac6ab45bdb0aea7e47362
mmustapha <mustaphamichael75@gmail.com>
2017-08-29 17:27:26 +0100

    Adding Toolbar to App \n Inorder to remove elevation

--- app/src/main/java/mmustapha/g_hub/Index/IndexActivity.java
+++ app/src/main/java/mmustapha/g_hub/Index/IndexActivity.java
@@ -2,6 +2,7 @@ package mmustapha.g_hub.Index;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import mmustapha.g_hub.R;
 
@@ -11,5 +12,7 @@ public class IndexActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
